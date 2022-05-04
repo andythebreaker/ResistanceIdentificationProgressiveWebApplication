53a309e2c6c185215e533a68014f15cbcaafeec3
andythebreaker <e24074724@mail.ncku.edu.tw>
2022-05-05 07:30:19 +0800

    vu8410

--- res-class-cv/src/component/KmeansBtn.jsx
+++ res-class-cv/src/component/KmeansBtn.jsx
@@ -5,16 +5,32 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import Stack from '@mui/material/Stack';
+import Stack from "@mui/material/Stack";
+import Chip from "@mui/material/Chip";
+import { KMeans } from "scikitjs";
+import { PieChart } from "react-minimal-pie-chart";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+var pixels = require("image-pixels");
 
 export class Welcome extends React.Component {
   constructor(props) {
     super(props);
+    console.log(props);
     this.state = {
       canvasA: {
         canvasWidth: 10,
         canvasHeight: 10,
         canvasURL: "./logo512.png", //前端可視路徑
+        pixW: -87,
+        pixH: -87,
+        nClustersT: props.nClustersT,
+        pieDataNum: [],
       },
     };
   }
@@ -25,6 +41,10 @@ export class Welcome extends React.Component {
         canvasWidth: 10,
         canvasHeight: 10,
         canvasURL: "./logo512.png", //前端可視路徑
+        pixW: -87,
+        pixH: -87,
+        nClustersT: this.state.canvasA.nClustersT,
+        pieDataNum: [],
       },
     });
   }
@@ -68,32 +88,77 @@ export class Welcome extends React.Component {
   }
 
   onchangeRefresh(et) {
-    console.log("onchangeRefresh");
-    console.log(
-      et.parentNode.parentNode.getElementsByClassName("rtW")[0].innerText
-    );
-    console.log(
-      et.parentNode.parentNode.getElementsByClassName("rtH")[0].innerText
-    );
-    console.log(
-      et.parentNode.parentNode.getElementsByClassName("rtU")[0].innerText
-    );
+    var tmp_stat = this.state.canvasA;
+    tmp_stat["canvasWidth"] =
+      et.parentNode.parentNode.getElementsByClassName("rtW")[0].innerText;
+    tmp_stat["canvasHeight"] =
+      et.parentNode.parentNode.getElementsByClassName("rtH")[0].innerText;
+    tmp_stat["canvasURL"] =
+      et.parentNode.parentNode.getElementsByClassName("rtU")[0].innerText;
     this.setState({
-      canvasA: {
-        canvasWidth: parseFloat(
-          et.parentNode.parentNode.getElementsByClassName("rtW")[0].innerText
-        ),
-        canvasHeight: parseFloat(
-          et.parentNode.parentNode.getElementsByClassName("rtH")[0].innerText
-        ),
-        canvasURL:
-          et.parentNode.parentNode.getElementsByClassName("rtU")[0].innerText,
-      },
+      canvasA: tmp_stat,
     });
   }
 
-  dokmeans(){
-      console.log("dokmeans");
+  dokmeans(et) {
+    console.log("dokmeans");
+    var imgurl =
+      et.parentNode.parentNode.getElementsByClassName("rtU")[0].innerText;
+    async function dokmeansAsync(t) {
+      function howMuchIsRepeated_es6(arr) {
+        //https://stackoverflow.com/questions/19395257/how-to-count-duplicate-value-in-an-array-in-javascript
+        // count is [ [valX, count], [valY, count], [valZ, count]... ];
+        const count = [...new Set(arr)].map((val) => [
+          val,
+          arr.join("").split(val).length - 1,
+        ]);
+
+        for (let i = 0; i < count.length; i++) {
+          console.log(`Value ${count[i][0]} is repeated ${count[i][1]} times`);
+
+          /////////////////////////////
+          var tmp_statz = t.state.canvasA;
+          tmp_statz["pieDataNum"].push({
+              //TODO:add color to pie chart
+            title: count[i][0],
+            value: count[i][1],
+            color: "#E38627",
+          });
+          t.setState({
+            canvasA: tmp_statz,
+          });
+          /////////////////////////////////
+        }
+      }
+      var { data, width, height } = await pixels(imgurl);
+      /////////////////////////////
+      var tmp_stat = t.state.canvasA;
+      tmp_stat["pixW"] = width;
+      tmp_stat["pixH"] = height;
+      t.setState({
+        canvasA: tmp_stat,
+      });
+      /////////////////////////////////
+      //console.log(Array.from(data));
+      //let X = Array.from(data);
+      const n = 4;
+      var ceil = Math.ceil;
+      let X = Array.from(Array(ceil(data.length / n)), (_, i) =>
+        data.slice(i * n, i * n + n - 1)
+      ); //https://stackoverflow.com/questions/8495687/split-array-into-chunks
+      console.log(X);
+      /*arr1 = [[1, 2], [3, 4]];
+      [ [ 1, 2 ], [ 3, 4 ] ]
+      > arr1.flat();
+      [ 1, 2, 3, 4 ]*/
+      const kmean = new KMeans({ nClusters: t.state.canvasA.nClustersT });
+      var kfp = kmean.fitPredict(X);
+      kfp.array().then(function (d) {
+        console.log(d);
+        howMuchIsRepeated_es6(d.flat());
+      });
+    }
+    dokmeansAsync(this);
   }
 
   render() {
@@ -135,8 +200,21 @@ export class Welcome extends React.Component {
                 }}
               />
               <Stack spacing={2} direction="row">
-              <Button variant="text" onClick={this.dokmeans}>dokmeans</Button>
-            </Stack>
+                <Button
+                  color="secondary"
+                  variant="text"
+                  style={{
+                    background: "#ddb98b",
+                    height: "0.1vh",
+                    padding: "10 0 0 0vh",
+                  }}
+                  onClick={(e) => {
+                    this.dokmeans(e.target);
+                  }}
+                >
+                  dokmeans
+                </Button>
+              </Stack>
               <Card sx={{ maxWidth: 275 }}>
                 <CardContent>
                   <Typography className="rtW">
@@ -148,7 +226,6 @@ export class Welcome extends React.Component {
                   <Typography className="rtU">
                     {this.state.canvasA.canvasURL}
                   </Typography>
-                  <Typography> 強制更新</Typography>
                 </CardContent>
                 <CardActions>
                   <Button
@@ -162,6 +239,30 @@ export class Welcome extends React.Component {
                   </Button>
                 </CardActions>
               </Card>
+            </td>
+            <td>
+              <Table>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>
+                      <Stack direction="row" spacing={1}>
+                        <Chip label="寬" variant="outlined" />
+                        <Chip label={this.state.canvasA.pixW} />
+                        <Chip label="高" variant="outlined" />
+                        <Chip label={this.state.canvasA.pixH} />
+                      </Stack>{" "}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>
+                      <PieChart
+                        style={{ height: "14vh" }}
+                        data={this.state.canvasA.pieDataNum}
+                      />
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
             </td>
           </tr>
         </tbody>
--- res-class-cv/src/component/TableColorCrecAll.jsx
+++ res-class-cv/src/component/TableColorCrecAll.jsx
@@ -5,20 +5,20 @@ export function TableCCall(props) {
   return <div className="tableCC">
     <table className="table table-striped table-bordered table-hover">
         <tbody>
-            <tr><td>背景</td><td><Welcome/></td></tr>
-            <tr><td>電阻本體</td><td><Welcome/></td></tr>
-            <tr><td>色塊1</td><td><Welcome/></td></tr>
-            <tr><td>色塊2</td><td><Welcome/></td></tr>
-            <tr><td>色塊3</td><td><Welcome/></td></tr>
-            <tr><td>色塊4</td><td><Welcome/></td></tr>
-            <tr><td>色塊5</td><td><Welcome/></td></tr>
-            <tr><td>色塊6</td><td><Welcome/></td></tr>
-            <tr><td>色塊7</td><td><Welcome/></td></tr>
-            <tr><td>色塊8</td><td><Welcome/></td></tr>
-            <tr><td>色塊9</td><td><Welcome/></td></tr>
-            <tr><td>色塊10</td><td><Welcome/></td></tr>
-            <tr><td>色塊11</td><td><Welcome/></td></tr>
-            <tr><td>色塊12</td><td><Welcome/></td></tr>
+            <tr><td>背景</td><td><Welcome nClustersT={5}/></td></tr>
+            <tr><td>電阻本體</td><td><Welcome nClustersT={8}/></td></tr>
+            <tr><td>色塊1</td><td><Welcome nClustersT={3}/></td></tr>
+            <tr><td>色塊2</td><td><Welcome nClustersT={3}/></td></tr>
+            <tr><td>色塊3</td><td><Welcome nClustersT={3}/></td></tr>
+            <tr><td>色塊4</td><td><Welcome nClustersT={3}/></td></tr>
+            <tr><td>色塊5</td><td><Welcome nClustersT={3}/></td></tr>
+            <tr><td>色塊6</td><td><Welcome nClustersT={3}/></td></tr>
+            <tr><td>色塊7</td><td><Welcome nClustersT={3}/></td></tr>
+            <tr><td>色塊8</td><td><Welcome nClustersT={3}/></td></tr>
+            <tr><td>色塊9</td><td><Welcome nClustersT={3}/></td></tr>
+            <tr><td>色塊10</td><td><Welcome nClustersT={3}/></td></tr>
+            <tr><td>色塊11</td><td><Welcome nClustersT={3}/></td></tr>
+            <tr><td>色塊12</td><td><Welcome nClustersT={3}/></td></tr>
         </tbody>
     </table>
   </div>;
