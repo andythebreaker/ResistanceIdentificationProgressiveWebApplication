6246b6e7f1f1785feb55d51d2d74268698a397a9
andythebreaker <e24074724@mail.ncku.edu.tw>
2022-05-17 17:20:32 +0800

    tfmd ok

--- res-class-cv/src/index.js
+++ res-class-cv/src/index.js
@@ -5,12 +5,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Webcam from "react-webcam";
+import { Tfmd } from "./Tfmd.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Webcam />
+    <Tfmd />
   </React.StrictMode>
 );
 
